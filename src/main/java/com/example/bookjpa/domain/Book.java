7b701e872b83a2e6c451d8a4988e535119f4d212
Sekator778@ <sekator778@gmail.com>
2022-09-03 14:52:35 +0200

    some log add

--- src/main/java/com/example/bookjpa/domain/Book.java
+++ src/main/java/com/example/bookjpa/domain/Book.java
@@ -2,13 +2,14 @@ package com.example.bookjpa.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-
+@Slf4j
 @Entity
 @Data
 @NoArgsConstructor
@@ -28,16 +29,19 @@ public class Book {
 
     public Book(String description) {
         this.description = description;
+        log.info("constructor class Book has created");
     }
 
     @PrePersist
     void onCreate() {
         this.setCreated(LocalDateTime.now());
         this.setModified(LocalDateTime.now());
+        log.info("method PrePersist done");
     }
 
     @PreUpdate
     void onUpdate() {
         this.setModified(LocalDateTime.now());
+        log.info("method PreUpdate use");
     }
 }
