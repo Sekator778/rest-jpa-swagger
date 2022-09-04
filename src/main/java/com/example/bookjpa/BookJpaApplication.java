6503282ffbe5b0da6dbed8bcc103f4b8bfb6fb86
Sekator778@ <sekator778@gmail.com>
2022-09-04 18:28:38 +0200

    add logback properties file

--- src/main/java/com/example/bookjpa/BookJpaApplication.java
+++ src/main/java/com/example/bookjpa/BookJpaApplication.java
@@ -2,8 +2,13 @@ package com.example.bookjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource(value = "file:app.properties",  ignoreResourceNotFound = true)
+})
 public class BookJpaApplication {
 
     public static void main(String[] args) {
